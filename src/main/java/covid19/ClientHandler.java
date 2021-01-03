40f31d893212ffffc42e886a3a82563489c40270
Igor Nedovic <igornedovic@gmail.com>
2021-01-03 12:50:24 +0100

    8

--- src/main/java/covid19/ClientHandler.java
+++ src/main/java/covid19/ClientHandler.java
@@ -2,7 +2,6 @@ package covid19;
 
 import java.io.*;
 import java.net.*;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -10,17 +9,17 @@ import java.util.concurrent.TimeUnit;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
 import org.json.simple.*;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonWriter;
 
 public class ClientHandler extends Thread {
-	
+
 	Socket soketZaKomunikaciju;
 	JSONArray nizIspitanika;
-	
+
 	FileWriter fajlUpisivac;
 	BufferedReader klijentInput;
 	PrintStream klijentOutput;
@@ -28,17 +27,16 @@ public class ClientHandler extends Thread {
 	JSONObject podaciCovid;
 	Gson gson = new GsonBuilder().setPrettyPrinting().create();
 	String username;
-	
+
 	public ClientHandler(Socket soketZaKomunikaciju, JSONArray nizIspitanika) {
 		this.soketZaKomunikaciju = soketZaKomunikaciju;
 		this.nizIspitanika = nizIspitanika;
 	}
-	
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
-		
+
 		try {
 			klijentInput = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
 			klijentOutput = new PrintStream(soketZaKomunikaciju.getOutputStream());
@@ -60,18 +58,23 @@ public class ClientHandler extends Thread {
 
 			} while (!input.equals("1") && !input.equals("2") && !input.equals("3"));
 
-			if (input.equals("1")) {
+			while (input.equals("1")) {
+
 				if (registracija()) {
 
 					klijentOutput.println("Uspesno ste se registrovali. Sada se mozete prijaviti na sistem.");
 					klijentOutput.println();
 
 					input = "2";
+				} else {
+					klijentOutput.println();
+					korisnickiMeni();
+					input = klijentInput.readLine();
 				}
-
 			}
 
 			if (input.equals("2")) {
+				int brojacPokusaja = 0;
 
 				boolean validacija = false;
 				while (!validacija) {
@@ -85,15 +88,15 @@ public class ClientHandler extends Thread {
 					for (Object korisnik : nizIspitanika) {
 						podaciCovid = (JSONObject) korisnik;
 
-						if (podaciCovid.get(username) != null) {							
+						if (podaciCovid.get(username) != null) {
 							podaciIspitanik = (JSONObject) podaciCovid.get(username);
-							
-							if((podaciCovid.get(username) == podaciCovid.get("admin")) && password.equals("admin")) {
+
+							if ((podaciCovid.get(username) == podaciCovid.get("admin")) && password.equals("admin")) {
 								adminPanel();
 								validacija = true;
 								break;
 							}
-							
+
 							if (!password.equals(podaciIspitanik.get("Password"))) {
 								klijentOutput.println("Pogresan password. Pokusajte ponovo");
 								break;
@@ -198,6 +201,12 @@ public class ClientHandler extends Thread {
 
 					if (podaciCovid.get(username) == null) {
 						klijentOutput.println("Uneli ste nepostojeci username! Pokusajte ponovo.");
+						brojacPokusaja++;
+						if(brojacPokusaja > 5) {
+							klijentOutput.println("Iskoristili ste sve pokusaje da se prijavite. "
+							+ "Ponovo pristupite sistemu nakon 30 minuta da biste se registrovali.");
+							return;
+						}
 					}
 				}
 			}
@@ -205,8 +214,7 @@ public class ClientHandler extends Thread {
 			fajlUpisivac = new FileWriter("covid19.txt");
 			fajlUpisivac.write(gson.toJson(nizIspitanika));
 			fajlUpisivac.flush();
-		
-			
+
 		} catch (IOException e) {
 			// ovde je pokriveno ako klijent nije napisao ***quit ali je stisnuo iksic,
 			// terminate, nestalo struje
@@ -218,81 +226,79 @@ public class ClientHandler extends Thread {
 			} catch (IOException e1) {
 				System.out.println("Greska prilikom upisivanja u bazu ispitanika.");
 			}
-		} 
-		
+		}
+
 	}
-	
+
 	public void korisnickiMeni() {
 		klijentOutput.println("KORISNICKI MENI:");
 		klijentOutput.println("1. Registracija");
 		klijentOutput.println("2. Prijava");
 		klijentOutput.println("3. Izlaz");
 		klijentOutput.println("Vas izbor:");
-		
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public boolean registracija() throws IOException {
 		klijentOutput.println("Unesite username: ");
 		username = klijentInput.readLine();
 
-		for(Object korisnik : nizIspitanika) {
+		for (Object korisnik : nizIspitanika) {
 			podaciCovid = (JSONObject) korisnik;
-			if(podaciCovid.get(username) != null) {
-				klijentOutput.println("Vec postoji registrovani korisnik sa datim korisnickim imenom! Unesite drugaciji username.");
+			if (podaciCovid.get(username) != null) {
+				klijentOutput.println(
+						"Vec postoji registrovani korisnik sa datim korisnickim imenom! Unesite drugaciji username.");
 				return false;
-			}	
-		}		
-		
+			}
+		}
+
 		klijentOutput.println("Unesite password: ");
 		String password = klijentInput.readLine();
 		podaciIspitanik.put("Password", password);
-		
+
 		klijentOutput.println("Unesite Vase ime: ");
 		String ime = klijentInput.readLine();
-		while(ime.equals("") || !ime.matches("^[a-zA-Z]*$") || ime.length()<1) {
+		while (ime.equals("") || !ime.matches("^[a-zA-Z]*$") || ime.length() < 1) {
 			klijentOutput.println("Uneli ste neispravno ime, pokusajte ponovo");
 			klijentOutput.println("Unesite Vase ime: ");
 			ime = klijentInput.readLine();
 		}
 		podaciIspitanik.put("Ime", ime);
 
-		
 		klijentOutput.println("Unesite Vase prezime: ");
 		String prezime = klijentInput.readLine();
-		while(prezime.equals("") || !prezime.matches("^[a-zA-Z]*$") || prezime.length()<1) {
+		while (prezime.equals("") || !prezime.matches("^[a-zA-Z]*$") || prezime.length() < 1) {
 			klijentOutput.println("Uneli ste neispravno prezime, pokusajte ponovo");
 			klijentOutput.println("Unesite Vase prezime: ");
 			prezime = klijentInput.readLine();
 		}
 		podaciIspitanik.put("Prezime", prezime);
-		
+
 		klijentOutput.println("Unesite Vas pol(M/Z): ");
 		String pol = (klijentInput.readLine()).toUpperCase();
-		while(!(pol.equals("M")) && !(pol.equals("Z"))) {
+		while (!(pol.equals("M")) && !(pol.equals("Z"))) {
 			klijentOutput.println("Uneli ste neispravnu oznaku pola, pokusajte ponovo");
 			klijentOutput.println("Unesite Vase pol: ");
 			pol = klijentInput.readLine();
 		}
 		podaciIspitanik.put("Pol", pol);
-		
+
 		klijentOutput.println("Unesite Vas email: ");
 		String email = klijentInput.readLine();
-		while(!(email.contains("@")) || !(email.contains("."))) {
+		while (!(email.contains("@")) || !(email.contains("."))) {
 			klijentOutput.println("Uneli ste neispravan email, pokusajte ponovo");
 			klijentOutput.println("Unesite Vase email: ");
 			email = klijentInput.readLine();
 		}
 		podaciIspitanik.put("Email", email);
-		
-		
+
 		podaciCovid.put(username, podaciIspitanik);
 		nizIspitanika.add(podaciCovid);
-		
-		
+
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void testSamoprocene() throws IOException {
 
@@ -373,22 +379,21 @@ public class ClientHandler extends Thread {
 		}
 
 	}
-	
+
 	public String zapisDatuma(GregorianCalendar datum) {
-		
+
 		int godina = datum.get(GregorianCalendar.YEAR);
 		int mesec = datum.get(GregorianCalendar.MONTH);
 		int dan = datum.get(GregorianCalendar.DAY_OF_MONTH);
 		int sat = datum.get(GregorianCalendar.HOUR_OF_DAY);
 		int minut = datum.get(GregorianCalendar.MINUTE);
 		int sekund = datum.get(GregorianCalendar.SECOND);
-		
-		
-		return (dan+"/"+(mesec+1)+"/"+godina+" "+sat+":"+minut+":"+sekund);
+
+		return (dan + "/" + (mesec + 1) + "/" + godina + " " + sat + ":" + minut + ":" + sekund);
 	}
-	
-	public long izracunajMilisekunde(String datum)  {	
-	
+
+	public long izracunajMilisekunde(String datum) {
+
 		DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
 		DateTime parsiranDatum = DateTime.parse(datum, formatter);
 		long milisekunde = parsiranDatum.getMillis();
@@ -397,8 +402,15 @@ public class ClientHandler extends Thread {
 
 	private void adminPanel() {
 		
+		klijentOutput.println();
+		klijentOutput.println("ADMINISTRATORSKI PANEL");
+		klijentOutput.println("---------------------------------------------------");
+		klijentOutput.println("Pregled statistike o virusu COVID19:");
+		klijentOutput.println("Ukupan broj testiranja: " +  Server.podaciAdmin.get("Broj testiranja"));
+		klijentOutput.println("Ukupan broj pozitivnih testova: " + Server.podaciAdmin.get("Broj pozitivnih testova"));
+		klijentOutput.println("Ukupan broj negativnih testova: " + Server.podaciAdmin.get("Broj negativnih testova"));
+		klijentOutput.println("Ukupan broj ispitanika pod nadzorom: " + Server.podaciAdmin.get("Broj ispitanika pod nadzorom"));
+		klijentOutput.println("---------------------------------------------------");
+
 	}
 }
-
-
-
